8a389614e51916e1cb7960aa75e9f7c30f4d0a7b
mahmoudkhalifakh91-arch <mahmoudkhalifa.kh91@gmail.com>
2026-09-21 13:51:11 +0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'app.dart';
+import 'firebase_options.dart';
+import 'services/push_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
+  runApp(const WasalahApp());
+}
